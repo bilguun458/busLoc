dcd7f559b83934ab1fd102ec598c65755dc19700
bjavkhlan <javkhlan96@gmail.com>
2017-12-15 14:50:22 +0800

    minor change

--- app_client/app.js
+++ app_client/app.js
@@ -17,3 +17,27 @@
 	.module('busLocApp')
 	.config(['$routeProvider', config]);
 })();
+
+
+window.onload = function() {
+
+    var ghRouting = new GraphHopper.Routing({
+      key: "326f0157-14d7-4b0c-8bb7-55edf7f7962b",
+      vehicle: "car",
+      elevation: false
+    });
+
+    ghRouting.addPoint(new GHInput(47.400905, 8.534317));
+    ghRouting.addPoint(new GHInput(47.394108, 8.538265));
+
+    ghRouting.doRequest()
+      .then(function(json) {
+        // Add your own result handling here
+        console.log(json);
+      })
+      .catch(function(err) {
+        console.error(err.message);
+      });
+
+
+ };	
